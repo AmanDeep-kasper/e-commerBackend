a7df58c634ac577cb99f43face03fc21f05c10d3
AmanDeep-kasper <amandeepempkasper@gmail.com>
2026-03-25 18:32:27 +0530

    update in code

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get("/", (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () =>
+app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`),
 );
